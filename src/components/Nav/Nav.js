04109b8ccd3506a6efea197fbf1c88028021ae2b
Skylar <skylarc55@gmail.com>
2023-03-20 19:55:19 -0400

    Add Nav component

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,11 @@
+import './Nav.scss';
+
+const Nav = () => {
+  return (
+    <div className="nav-container">
+      <button type="button" className="add-button"><div className="add-plus"></div></button>
+    </div>
+  )
+}
+
+export default Nav;
