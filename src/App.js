d99f8f06a83aebaf1a7184beb5d076e63282a59b
Skylar <skylarc55@gmail.com>
2023-03-20 19:49:03 -0400

    Add bottom nav bar with add button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import Dashboard from './components/Dashboard/Dashboard';
 import SignUp from './components/SignUp/SignUp';
 import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
 import { useSelector, useDispatch } from 'react-redux';
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
         <Dashboard /> :
         <SignUp />
       }
+      <Nav />
     </>
   );
 }
