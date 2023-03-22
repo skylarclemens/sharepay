1172679844e5dd47f7310e3b641d056a7fc671bd
Skylar <skylarc55@gmail.com>
2023-03-22 16:51:28 -0400

    Add Sign Up page and integrate Supabase sign up

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -2,32 +2,47 @@ import './SignUp.scss';
 import { useSelector, useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { setUser } from '../../slices/userSlice';
+import { supabase } from '../../supabaseClient';
 
 const SignUp = () => {
-  const user = useSelector(state => state.user);
-  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
 
   const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [fullName, setFullName] = useState('');
 
-  const handleSignUp = (e) => {
+  const handleSignUp = async (e) => {
     e.preventDefault();
-    const newUser = {
-      id: crypto.randomUUID(),
-      email: email,
-      name: name,
-      groups: []
+    
+    try {
+      setLoading(true);
+      const { data, error } = await supabase.auth.signUp({
+        email: email,
+        password: password,
+        options: {
+          data: {
+            name: fullName
+          }
+        }
+      });
+      if (error) throw error;
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
-    dispatch(setUser(newUser));
   }
 
   return (
     <div className="sign-up">
-      <form className="sign-up-form" onSubmit={handleSignUp}>
-        <input type="email" value={email} placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-        <input type="text" value={name} placeholder ="Name" onChange={(e) => setName(e.target.value)} />
-        <button type="submit">Sign Up</button>
-      </form>
+      {loading ? 'Signing up...' :
+        (<form className="sign-up-form" onSubmit={handleSignUp}>
+          <input type="email" value={email} placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+          <input type="password" value={password} placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+          <input type="text" value={fullName} placeholder="Full name" onChange={(e) => setFullName(e.target.value)} />
+          <button className="button" type="submit">Sign Up</button>
+        </form>
+      )}
     </div>
   )
 }
