6a0baba65b9ce76337e1c6f63befc0a127440c01
Skylar <skylarc55@gmail.com>
2023-04-07 19:01:55 -0400

    Add form validation to NewExpense

--- src/pages/NewExpense/NewExpense.js
+++ src/pages/NewExpense/NewExpense.js
@@ -20,6 +20,7 @@ const NewExpense = () => {
   const [split, setSplit] = useState('');
   const [options, setOptions] = useState([]);
   const [friendSelected, setFriendSelected] = useState(null);
+  const [fieldErrors, setFieldErrors] = useState({});
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -39,6 +40,9 @@ const NewExpense = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setFieldErrors({...fieldErrors, formValid: true});
+
+    if (!handleValidation()) return;
 
     let expenseData;
     const newExpense = {
@@ -83,6 +87,36 @@ const NewExpense = () => {
     navigate(-1);
   }
 
+  const handleValidation = () => {
+    const errors = {}
+    let formValid = true;
+
+    if(!description) {
+      formValid = false;
+      errors.description = "Cannot be empty.";
+    }
+    if(!amount) {
+      formValid = false;
+      errors.amount = "Please enter an amount.";
+    }
+    if(!splitWith) {
+      formValid = false;
+      errors.splitWith = "Choose somebody to split the expense with.";
+    }
+    if(!paidBy) {
+      formValid = false;
+      errors.paidBy = "Select who paid the expense.";
+    }
+    if(!split) {
+      formValid = false;
+      errors.split = "Choose how you want to split the bill.";
+    }
+
+    errors.formValid = formValid;
+    setFieldErrors(errors);
+    return formValid;
+  } 
+
   const handleFriendSelect = (value) => {
     const friendSplit = friends.find(friend => friend.id === value);
     setFriendSelected(friendSplit);
@@ -95,18 +129,21 @@ const NewExpense = () => {
         <form className="expense-form" onSubmit={handleSubmit}>
           <div className="expense-input">
             <label className="input-label" htmlFor="description">Description</label>
-            <input id="description" name="description" type="text" className="text-field-input" value={description} onChange={(e) => setDescription(e.target.value)} />
+            <input id="description" name="description" type="text" className={`text-field-input ${fieldErrors.description && 'field-error'}`} value={description} onFocus={() => setFieldErrors({...fieldErrors, description: null})} onChange={(e) => setDescription(e.target.value)} />
+            {fieldErrors.description && <span className="field-error-text">{fieldErrors.description}</span>}
           </div>
           <div className="expense-input">
             <label className="input-label" htmlFor="amount">Amount</label>
-            <input id="amount" name="amount" type="number" className="text-field-input" value={amount} onChange={(e) => setAmount(e.target.value)} />
+            <input id="amount" name="amount" type="number" className={`text-field-input ${fieldErrors.amount && 'field-error'}`} value={amount} onFocus={() => setFieldErrors({...fieldErrors, amount: null})} onChange={(e) => setAmount(e.target.value)} />
+            {fieldErrors.amount && <span className="field-error-text">{fieldErrors.amount}</span>}
           </div>
           <div className="expense-input">
             <span className="input-label">Split between</span>
-            <Select options={options} onChange={(opt) => {
+            <Select options={options} onFocus={() => setFieldErrors({...fieldErrors, splitWith: null})} onChange={(opt) => {
               setSplitWith(opt.value)
               handleFriendSelect(opt.value)
             }}/>
+            {fieldErrors.splitWith && <span className="field-error-text">{fieldErrors.splitWith}</span>}
           </div>
           {friendSelected && (
             <>
@@ -114,17 +151,24 @@ const NewExpense = () => {
                 <legend className="input-label">Paid By</legend>
                 <div className="expense-radio radio-paid-by">
                   <div className="radio-option">
-                    <div className={`user-detail-select ${paidBy === user.id ? 'selected' : ''}`} onClick={() => setPaidBy(user.id)}>
+                    <div className={`user-detail-select ${paidBy === user.id ? 'selected' : ''}`} onClick={() => {
+                      setPaidBy(user.id)
+                      setFieldErrors({...fieldErrors, paidBy: null})
+                    }}>
                       <Avatar url={account?.avatar_url} size={34} classes="white-border"/>
                       Me
                     </div>
                   </div>
                   <div className="radio-option">
-                    <div className={`user-detail-select ${paidBy === friendSelected.id ? 'selected' : ''}`} onClick={() => setPaidBy(friendSelected.id)}>
+                    <div className={`user-detail-select ${paidBy === friendSelected.id ? 'selected' : ''}`} onClick={() => {
+                      setPaidBy(friendSelected.id)
+                      setFieldErrors({...fieldErrors, paidBy: null})
+                    }}>
                       <Avatar url={friendSelected?.avatar_url} size={34} classes="white-border"/>
                       {friendSelected.name}
                     </div>
                   </div>
+                  {fieldErrors.paidBy && <span className="field-error-text">{fieldErrors.paidBy}</span>}
                 </div>
               </fieldset>
               <fieldset>
@@ -132,21 +176,23 @@ const NewExpense = () => {
                 <div className="expense-radio">
                   <div className="radio-option">
                     <label>
-                      <input id="split-equally" name="expense-split" type="radio" value="EQUALLY" checked={split === 'EQUALLY'} onChange={(e) => setSplit(e.target.value)} />
+                      <input id="split-equally" name="expense-split" type="radio" value="EQUALLY" checked={split === 'EQUALLY'} onFocus={() => setFieldErrors({...fieldErrors, split: null})} onChange={(e) => setSplit(e.target.value)} />
                       Split equally
                     </label>
                   </div>
                   <div className="radio-option">
                     <label>
-                      <input id="them-split-equally" name="expense-split" type="radio" value="FULL_AMOUNT" checked={split === 'FULL_AMOUNT'} onChange={(e) => setSplit(e.target.value)} />
+                      <input id="them-split-equally" name="expense-split" type="radio" value="FULL_AMOUNT" checked={split === 'FULL_AMOUNT'} onFocus={() => setFieldErrors({...fieldErrors, split: null})} onChange={(e) => setSplit(e.target.value)} />
                       Owed full amount
                     </label>
                   </div>
+                  {fieldErrors.split && <span className="field-error-text split-field">{fieldErrors.split}</span>}
                 </div>
               </fieldset>
             </>
           )}
           <button className="button" type="submit" alt="Create expense" title="Create expense">Create</button>
+          {fieldErrors.formValid === false && <span className="field-error-text .form-validation">Please fix the errors to submit expense.</span>}
         </form>
       </div>
     </div>
