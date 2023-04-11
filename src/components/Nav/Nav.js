deff34867edaba68dd4502b6e380a07d61415098
Skylar <skylarc55@gmail.com>
2023-04-11 13:48:26 -0400

    Add style for Nav button current page

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,25 +3,26 @@ import homeImg from '../../images/Home.svg';
 import groupImg from '../../images/Group.svg';
 import graphImg from '../../images/Graph.svg';
 import profileImg from '../../images/Profile.svg';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Nav = () => {
+  const location = useLocation();
+  const selectedPath = (linkPath) => location.pathname === linkPath && 'selected';
+
   return (
     <div className="nav-container">
       <div className="nav-buttons-container">
         <div className="nav-buttons nav-buttons-left">
-          <Link className="nav-button" to="/">
+          <Link className={`nav-button ${selectedPath("/")}`} to="/">
             <img src={homeImg} alt="Home icon"/>
           </Link>
-          <Link className="nav-button" to="/friends">
+          <Link className={`nav-button ${selectedPath("/friends")}`} to="/friends">
             <img src={groupImg} alt="Friends icon"/>
           </Link>
         </div>
         <div className="nav-buttons nav-buttons-right">
-          <Link className="nav-button" to="/">
-            <img src={graphImg} alt="Graph icon"/>
-          </Link>
-          <Link className="nav-button" to="/account">
+          <div className="nav-button"></div>
+          <Link className={`nav-button ${selectedPath("/account")}`} to="/account">
             <img src={profileImg} alt="Account icon"/>
           </Link>
         </div>
