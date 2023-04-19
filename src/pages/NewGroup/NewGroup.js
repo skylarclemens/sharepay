82971d7f42dc5426e2a7634dcb19090b6347f3ac
Skylar <skylarc55@gmail.com>
2023-04-19 16:29:44 -0400

    Add UserButton and update Add Group Member button styling

--- src/pages/NewGroup/NewGroup.js
+++ src/pages/NewGroup/NewGroup.js
@@ -1,13 +1,16 @@
 import './NewGroup.scss';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
 import Header from '../../components/Header/Header';
 import TextInput from '../../components/Input/TextInput/TextInput';
 import Modal from '../../components/Modal/Modal';
 import SelectFriends from '../../components/SelectFriends/SelectFriends';
+import UserButton from '../../components/User/UserButton/UserButton';
 
 const NewGroup = () => {
+  const account = useSelector(state => state.account.data);
   const [groupName, setGroupName] = useState('');
-  const [groupMembers, setGroupMembers] = useState([]);
+  const [groupMembers, setGroupMembers] = useState([{...account}]);
   const [openSelectFriends, setOpenSelectFriends] = useState(false);
 
   const handleSubmit = (e) => {
@@ -23,11 +26,22 @@ const NewGroup = () => {
     <>
       <div className="new-group-container">
         <Header type="title" title="Create group" />
-        <form className="group-form group-input-container" onSubmit={handleSubmit}>
-          <TextInput name="name" label="Group name" value={groupName} onChange={(e) => setGroupName(e.target.value)} />
+        <form className="group-form" onSubmit={handleSubmit}>
+          <TextInput className="group-spacing" name="name" label="Group name" value={groupName} onChange={(e) => setGroupName(e.target.value)} />
           <div className="add-friends input-container">
-            <span className="input-label">Group members</span>
-            <button className="button" onClick={() => setOpenSelectFriends(true)}>Add friend</button>
+            <span className="input-label group-spacing">Group members</span>
+            <div className="group-members">
+              {groupMembers.map((member) => {
+                return (
+                  <UserButton key={member.id}
+                    user={member}
+                    name={member.name}
+                    variant="white"
+                  />
+                )
+              })}
+              <button className="friend-add-button button--icon" onClick={() => setOpenSelectFriends(true)}><div className="friend-add-plus"></div></button>
+            </div>
           </div>
         </form>
       </div>
