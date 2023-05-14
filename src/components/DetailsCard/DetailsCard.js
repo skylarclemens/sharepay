ae1d6140da2ab8ba1cad4e391af4eb92e2db7fcb
Skylar <skylarc55@gmail.com>
2023-05-14 12:55:47 -0400

    Add new Details card, Balance, Transactions to Profile

---
+++ src/components/DetailsCard/DetailsCard.js
@@ -0,0 +1,27 @@
+import './DetailsCard.scss';
+import Avatar from '../Avatar/Avatar';
+import FriendAction from '../../pages/FriendAction/FriendAction';
+
+const DetailsCard = ({ user, actions }) => {
+  return (
+    <>
+      <div className="details-card-background"></div>
+      <div className="details-card">
+        <div className="details__user">
+          <div className="details__avatar">
+            <Avatar url={user?.avatar_url} classes="white-border" size={65} />
+          </div>
+          <div className="details__info-text">
+            <h1 className="details__name">{user?.name}</h1>
+            <p className="details__username">{user?.email}</p>
+          </div>
+          <div className="details__actions">
+            {actions}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default DetailsCard;
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,38 +1,83 @@
-import { useParams } from 'react-router-dom';
+
 import './Profile.scss';
-import { useGetAccountQuery } from '../../slices/accountSlice';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import Header from '../../components/Header/Header';
-import Avatar from '../../components/Avatar/Avatar';
+import DetailsCard from '../../components/DetailsCard/DetailsCard';
 import FriendAction from '../FriendAction/FriendAction';
+import Transactions from '../../components/Transactions/Transactions';
+import { useGetDebtsQuery, selectSharedDebtsByFriendId } from '../../slices/debtSlice';
+import { useGetAccountQuery } from '../../slices/accountSlice';
+import { balanceCalc } from '../../helpers/balance';
+import { formatMoney } from '../../helpers/money';
+import receiveImg from '../../images/Receive.svg';
+import sendImg from '../../images/Send.svg';
 
 const Profile = () => {
+  const user = useSelector(state => state.auth.user);
   let { id } = useParams();
   const {
     data: profileUser,
     isSuccess: profileUserFetched,
   } = useGetAccountQuery(id);
+  const [balances, setBalances] = useState({ total: 0, owed: 0, owe: 0 });
+
+  const { sharedDebts,
+    isLoading: debtsLoading,
+    isSuccess: debtsFetched } = useGetDebtsQuery(undefined, {
+    selectFromResult: (result) => ({
+      ...result,
+      sharedDebts: selectSharedDebtsByFriendId(result, id)
+    })
+  })
+
+  useEffect(() => {
+    setBalances(balanceCalc(sharedDebts, user.id));
+  }, [sharedDebts, user]);
 
   return (
     <>
       <Header type="title"/>
       {profileUserFetched && (
         <div className="profile">
-          <div className="profile-info">
-            <div className="profile__user">
-              <div className="profile__avatar">
-                <Avatar url={profileUser?.avatar_url} classes="white-border" size={50} />
-              </div>
-              <div className="profile__info-text">
-                <h1 className="profile__name">{profileUser?.name}</h1>
-                <p className="profile__username">{profileUser?.email}</p>
-              </div>
+          <DetailsCard user={profileUser} actions={
+            <>
+            <div className="details__actions--left">
+              <FriendAction friend={profileUser} />
+              <button className="button button--small button--border-none button--flat">Add expense</button>
             </div>
-            <div className="profile__friend-actions">
-              <div className="profile__friend-actions--left">
-                <FriendAction friend={profileUser} />
-                <button className="button button--small button--border-none button--flat">Add expense</button>
+            <button className="button button--small button--border-none button--flat">Pay up</button>
+            </>
+          } />
+          <div className="profile__section profile__section--balance">
+            <h2>Balance</h2>
+            <div className="balance__items">
+              <div className="balance__item balance__item--owed">
+                <h3 className="balance__title">
+                  You're owed
+                  <img src={receiveImg} alt="Green arrow pointing bottom left" />
+                </h3>
+                <p className="balance__amount">{formatMoney(balances?.owed, false)}</p>
               </div>
-              <button className="button button--small button--border-none button--flat">Pay up</button>
+              <div className="balance__item balance__item--owe">
+                <h3 className="balance__title">
+                  You owe
+                  <img src={sendImg} alt="Red arrow pointing top right" />
+                </h3>
+                <p className="balance__amount">{formatMoney(balances?.owe, false)}</p>
+            </div>
+            </div>
+          </div>
+          <div className="profile__section profile__section--transactions">
+            <h2>Transactions</h2>
+            <div className="profile__transactions">
+              {debtsLoading &&
+                <div className="medium-gray">Loading...</div>}
+              {debtsFetched && balances.total !== 0 &&
+                <Transactions transactions={sharedDebts} type="debt" />}
+              {balances.total === 0 &&
+                <div className="medium-gray">No transactions available</div>}
             </div>
           </div>
         </div>
