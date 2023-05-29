135f6ee103861a8937bc467aa96c5d64dcc30f54
Skylar <skylarc55@gmail.com>
2023-05-29 15:59:17 -0400

    Add Recurring icon and button to adjust frequency for recurring expenses

--- src/pages/NewExpense/NewExpenseDetails/NewExpenseDetails.js
+++ src/pages/NewExpense/NewExpenseDetails/NewExpenseDetails.js
@@ -4,6 +4,7 @@ import DropdownSelect from '../../../components/Input/DropdownSelect/DropdownSel
 import TextInput from '../../../components/Input/TextInput/TextInput';
 import AmountInput from '../../../components/Input/AmountInput/AmountInput';
 import DateInput from '../../../components/Input/DateInput/DateInput';
+import recurringImg from '../../../images/Recurring.svg';
 
 import { CATEGORIES } from '../../../constants/categories';
 
@@ -11,6 +12,7 @@ const NewExpenseDetails = ({ description, setDescription, amount, setAmount, fie
   const [categoryImg, setCategoryImg] = useState(null);
   const [category, setCategory] = useState('category');
   const [dateVal, setDateVal] = useState(new Date());
+  const [recurringVal, setRecurringVal] = useState('Once');
 
   const categoriesList = useRef(Object.keys(CATEGORIES))?.current?.map(category => {
     return {
@@ -28,6 +30,13 @@ const NewExpenseDetails = ({ description, setDescription, amount, setAmount, fie
     const validInput = value.match(/^(\d*\.{0,1}\d{0,2}$)/);
     if (validInput) setAmount(value);
   };
+
+  const handleRecurring = () => {
+    const recurringOptions = ['Once', 'Daily', 'Weekly', 'Monthly', 'Yearly'];
+    const currentRecurringIndex = recurringOptions.indexOf(recurringVal);
+    const nextRecurringIndex = (currentRecurringIndex + 1) % recurringOptions.length;
+    setRecurringVal(recurringOptions[nextRecurringIndex]);
+  }
 
   const handleValidation = () => {
     const errors = {};
@@ -104,6 +113,14 @@ const NewExpenseDetails = ({ description, setDescription, amount, setAmount, fie
             dateFormat={'M/d/yy'}
             onChange={(date) => setDateVal(date)}
           />
+          <div className="recurring" onClick={
+            () => handleRecurring()
+          }>
+            <img src={recurringImg} alt="Recurring icon" height="18" width="18"/>
+            <div className="recurring-select">
+              {recurringVal}
+            </div>
+          </div>
         </div>
         <button type="button" className="button button--flat button--medium" onClick={() => {
         if(!handleValidation()) return;
