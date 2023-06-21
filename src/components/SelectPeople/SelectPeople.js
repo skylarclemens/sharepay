b7c6164edd4741bcb1397480452cc41e8ace24a3
Skylar <skylarc55@gmail.com>
2023-06-21 16:12:35 -0400

    Add new section to show users selected in SelectPeople

--- src/components/SelectPeople/SelectPeople.js
+++ src/components/SelectPeople/SelectPeople.js
@@ -8,8 +8,10 @@ import Search from '../Search/Search';
 import { useGetFriendsQuery, selectFriendsBySearchQuery } from '../../slices/friendSlice';
 import { useGetGroupsQuery, selectGroupsBySearchQuery } from '../../slices/groupSlice';
 import { useDebounce } from '../../hooks/useDebounce';
+import Avatar from '../Avatar/Avatar';
+import removeImg from '../../images/Remove.svg';
 
-const SelectPeople = ({ showGroups = false, handleAdd, existingUsers = [] }) => {
+const SelectPeople = ({ showGroups = false, handleAdd, existingUsers = [], setOpen }) => {
   const [value, setValue] = useState('');
   const debouncedSearchValue = useDebounce(value, 500);
 
@@ -72,7 +74,25 @@ const SelectPeople = ({ showGroups = false, handleAdd, existingUsers = [] }) => 
 
   return (
     <div className="add-friend-container select-friends">
-      <Header type="title" title="Select people" />
+      <Header type="title" title="Select people" classes="transparent" headerLeftFn={() => setOpen(false)} />
+      <div className="split-with-people">
+        <div className="split-with-people__heading">
+          <span className="section-heading">Split with</span>
+          <button className="button--no-style clear-button" onClick={() => setSelectedUsers([])}>Clear</button>
+        </div>
+        <div className="split-with-people__users">
+        {selectedUsers.map(user => {
+          return (
+            <div key={`search-${user.id}`} className="split-with-user">
+              <Avatar url={user?.avatar_url} classes="white-border" />
+              <span>{user?.name}</span>
+              <button className="button--no-style remove-user">
+                <img src={removeImg} alt="Remove user" onClick={() => setSelectedUsers(selectedUsers.filter(selected => selected.id !== user.id))} />
+              </button>
+            </div>
+          )})}
+          </div>
+      </div>
       <Search value={value} setValue={setValue} suggestions={renderSuggestions} />
       <button className={`button button--floating selected-users-button ${selectedUsers.length > 0 ? 'show' : ''}`} onClick={() => handleAdd(selectedUsers)}>{`Add friend${selectedUsers.length > 1 ? 's' : ''}`}</button>
     </div>
