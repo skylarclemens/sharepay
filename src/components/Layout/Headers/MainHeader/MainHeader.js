dec41ca26baa369a41371b20c1b7b3761b65969c
Skylar <skylarc55@gmail.com>
2023-06-27 10:43:42 -0400

    Change Layout Green color

--- src/components/Layout/Headers/MainHeader/MainHeader.js
+++ src/components/Layout/Headers/MainHeader/MainHeader.js
@@ -2,7 +2,7 @@ import Header from "../Header/Header";
 
 const MainHeader = ({ title, right, rightFn, className, ...props }) => {
   return (
-    <Header type="main" className={className} backgroundColor={'#729B79'} left={title} right={right} {...props} />
+    <Header type="main" className={className} left={title} right={right} {...props} />
   )
 }
 
