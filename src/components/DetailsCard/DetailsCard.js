387e69c3317da37e744260209243ab7a533fc808
Skylar <skylarc55@gmail.com>
2023-07-01 18:42:14 -0400

    Add category to Expense page

--- src/components/DetailsCard/DetailsCard.js
+++ src/components/DetailsCard/DetailsCard.js
@@ -1,16 +1,23 @@
 import './DetailsCard.scss';
 import Avatar from '../Avatar/Avatar';
 import Skeleton from '../Skeleton/Skeleton';
+import Icon from '../Icons/Icon';
+import { CATEGORIES } from '../../constants/categories';
 
-const DetailsCard = ({ title, subTitle, avatarUrl = null, actions, type = 'user', classes, skeleton = false, children }) => {
+const DetailsCard = ({ title, subTitle, category = null, avatarUrl = null, actions, type = 'user', classes, skeleton = false, children }) => {
   return (
     <>
       <div className={`details-card-background ${classes}`}></div>
       <div className="details-card">
         <div className="details__user">
-          {type !== 'expense' && <div className="details__avatar">
+          {type !== 'expense' ? <div className="details__avatar">
             <Avatar url={avatarUrl} classes="white-border white-border--thick" size={65} type={type} />
-          </div>}
+          </div> : <div className="details__icon"
+            style={{
+              background: CATEGORIES[category]?.color,
+            }}>
+            <Icon name={category} height="40" width="40" />
+          </div> }
           {skeleton ? (
           <Skeleton height={50} width={300} />
             ) : (
